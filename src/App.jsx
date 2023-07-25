cf284f56a2d27e24ba4ae018bb3162cfadc768e7
Rachana Das <101170210+rachana1792@users.noreply.github.com>
2023-07-25 15:58:11 +0530

    derived atoms

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import './App.css'
 import TodoAdd from "./components/TodoAdd"; 
 import { useAtom } from "jotai";
 import TodoList from "./components/TodoList"; 
-import { todoAtom } from "./TodoStore"; 
+import { todoAtom, textUpperCaseAtom } from "./TodoStore";
 
 const App = () => {
   const [todos] = useAtom(todoAtom); 
+  const [title] = useAtom(textUpperCaseAtom); 
 
   return (
     <div className="App">
+      <h1>{title}</h1>
       {todos.map((duty) => (
         <TodoList key={duty.id} task={duty} />
       ))}
--- src/TodoStore.jsx
+++ src/TodoStore.jsx
@@ -1,4 +1,8 @@
 import { atom } from "jotai";
 
 export const todoAtom = atom([]);
-export const atomTitle = 'todo'
+const textAtom = atom('Todo')
+export const textUpperCaseAtom = atom(
+  (get) => get(textAtom).toUpperCase(),
+  (_get, set, newText) => set(textAtom, newText)
+)
